01dc7bb54c30067b8de6b7caa0513c5338ecaa5e
Alejandro Soto Garcia <alejandro.soto@iMac-de-web.local>
2020-08-25 11:02:03 -0500

    se sube primer app con hook 💻

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
-    const handleAdd = (e) => {
-        console.log(e)
+    const [ counter, setCounter ] = useState(value); // Destructuro el hook de useState que viene el valor y funcion del Valor
+    
+
+    const handleAdd = e => setCounter(counter + 1);
+
+    const handleSubstract = (e) => {
+        setCounter( c => c - 1 );
     }
+    const handleReset = e => setCounter(value);
 
     return (
         <Fragment>
             <h1>Counter App</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
             <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
         </Fragment>
     )
 }
