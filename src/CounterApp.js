4c26adcd78a279245d5ba438855b52b13d7c3e8c
Alejandro <alexsotog82@gmail.com>
2020-08-24 23:05:15 -0500

    se sube 💻 eventos en react 🦾

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,11 +2,16 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
     return (
         <Fragment>
             <h1>Counter App</h1>
             <h2>{ value }</h2>
-            <button>+1</button>
+            <button onClick={ handleAdd }>+1</button>
         </Fragment>
     )
 }
